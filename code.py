54f5d06d6ac14952b2b9735f2c95baeb14c3f7e3
abhit17 <31434088+abhit17@users.noreply.github.com>
2018-12-02 12:30:41 +0530

    Update code.py

--- code.py
+++ code.py
@@ -22,7 +22,6 @@ y = np.array(data['Close'])
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 #Linear Rregression
-
 lrmod = linear_model.LinearRegression()
 lrmod.fit(x_train,y_train)
 y_pred = lrmod.predict(x_test)
@@ -30,7 +29,6 @@ print("Mean squared error lr: %f" % mean_squared_error(y_test, y_pred))
 
 
 #Logistic Rregression
-
 clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
@@ -38,14 +36,12 @@ print("Mean squared error lgr: %f" % mean_squared_error(y_test, y_pred))
 
 
 #kNN
-
 neigh = KNeighborsClassifier(n_neighbors=3)
 neigh.fit(x_train, y_train)
 y_pred = neigh.predict(x_test)
 print("Mean squared error knn: %f" % mean_squared_error(y_test, y_pred))
 
 #Decision Tree
-
 clf = tree.DecisionTreeClassifier()
 clf.fit(x_train, y_train)
 print("Mean squared error dt: %f" % mean_squared_error(y_test, y_pred))
